69979aba53bd7d769737a5f2e193db7927e0ff82
Jon Huh <oneandwholly@gmail.com>
2017-04-18 18:05:33 +0900

    Get text value from SearchBar into the component state using event handler function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+  // a class component must have render method.
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+  }
+
+  // event handler method
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
 
 export default SearchBar;
