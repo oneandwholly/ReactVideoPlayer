b0189c57f8769dd3878ecf711aa8cb2fbf0306f7
Jon Huh <oneandwholly@gmail.com>
2017-04-18 17:25:53 +0900

    Get SearchBar component to display onto the page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDjSt3ZNqQ6V0BEPyHECwRD3egzqV8Cxc8';
 
 // Create a new component, and this comonent should produce some HTML.
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 // Take this component's generated HTML and put it on to the page(in the DOM)
